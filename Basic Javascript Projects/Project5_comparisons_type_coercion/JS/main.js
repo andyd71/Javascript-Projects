252055ea7c15daf6d3e8afeeb64d8f4394984580
andyd71 <anfield250271@hotmail.com>
2021-02-10 18:12:18 +0000

    Project 5

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,37 @@
+document.write(typeof "Andy",'<p>')
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_Function1() {
+    document.getElementById("Test1").innerHTML = isNaN('Hello there');
+
+}
+
+function my_Function2() {
+    document.getElementById("Test2").innerHTML = isNaN('555');
+
+}
+
+function number_1() {
+    document.getElementById(Num1).innerHTML =1.7976931348623157E309;
+}
+
+function number_2() {
+    document.getElementById(Num2).innerHTML =-1.7976931348623157E309;
+}
+
+document.write(6 > 3, '<p>');
+
+document.write(6 < 3, '<p>' );
+
+console.log(6+5);
+
+document.write(6 + "5", '<p>' );
+
+console.log(6 < 5);
+
+document.write(10 == 10, '<p>' );
+
+document.write(3 == 10, '<p>')
