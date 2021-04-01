ff9b88e870635db72939dfa9d2eb8b7e71499a59
andyd71 <68546963+andyd71@users.noreply.github.com>
2021-04-01 16:09:21 +0100

    Add files via upload

--- JavaScript Projects/JavaScript/JavaScript_2.js
+++ JavaScript Projects/JavaScript/JavaScript_2.js
@@ -1,17 +1,21 @@
 function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
-    if (x == null || x == "") {
+    var y = document.forms["myForm"]["lname"].value;
+    if ( x.fname == "") {
         alert("First name must be filled out");
         return false;
     }
+    if( y == "") {
+        alert("Last name must be filled out");
+        return false;
+    
     }
-    function validateForm() {
-        var x = document.forms["myForm"]["lname"].value;
-        if (x == null || x == "") {
-            alert("Last name must be filled out");
-            return false;
-        }
-        }
+}
+
+
+   
+    
+        
     
     
     
