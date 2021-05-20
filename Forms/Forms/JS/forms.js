3193f62e1567568c181f341ab63c98934e79551a
andyd71 <68546963+andyd71@users.noreply.github.com>
2021-05-20 12:40:21 +0100

    Add files via upload

--- Forms/Forms/JS/forms.js
+++ Forms/Forms/JS/forms.js
@@ -1,43 +1,38 @@
-var currentTab = 0; //current tab is set to be first tab.
-showTab(currentTab);//display the current tab.
+function validateForm() {
+    var fname = document.forms["regForm"]["fname"].value;
+    var lname = document.forms["regForm"]["lname"].value;
+    var mail = document.forms["regForm"]["email"].value;
+    var ring = document.forms["regForm"]["phone"].value;
+    var bday = document.forms["regForm"]["birthday"].value;
+    var uname = document.forms["regForm"]["username"].value;
+    var pass = document.forms["regForm"]["password"].value;
 
-function showTab(n) {
-    //this function will display the specified tab of the form
-    var x = document.getElementsByClassName("tab");
-    if (n == 1 && !validateForm()) return false;
-    //hide the current tab
-    x[currentTab].style.display = "none";
-    //increase or decrease current tab by 1
-    currentTab = currentTab + n;
-    //if you have reached the end of the form.....
-    if (currentTab >= x.length) {
-        //...the form gets submitted.
-        document.getElementById("regForm").submit();
+    if (fname == "") {
+        alert("First name must be filled out");
         return false;
     }
-    //otherwise display the correct tab.
-    showTab(currentTab);
-}
-
-function validateForm() {
-    //this function deals with the validation of the form fields.
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
-    //a loop that checks every input field in the current tab.
-    for (i = 0; i < y.length; i++) {
-        //if a field is empty....
-        if (y[i].value == "") {
-            //add an invalid class to the field.
-            y[i].className += " invalid";
-            //and set the current valid status to false.
-            valid = false;
-        }
+    if (lname == "") {
+        alert("Last name must be filled out");
+        return false;
     }
-    //if the valid status is true, mark the step as finished and valid.
-    if (valid) {
-    [currentTab].className += " finish"; 
+    if (mail == "") {
+        alert("Email must be filled out");
+        return false;
     }
-    return valid;//return the valid status.
+    if (ring == "") {
+        alert("Phone number must be filled out");
+        return false;
+    }
+    if (bday == "") {
+        alert("Birthday must be filled out");
+        return false;
+    }
+    if (uname == "") {
+        alert("Username must be filled out");
+        return false;
+    }
+    if (pass == "") {
+        alert("Password must be filled out");
+        return false;
+    }
 }
-
