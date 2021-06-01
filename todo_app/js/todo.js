811a8a56201d9790c54937585300caf1b1d7b9c8
andyd71 <68546963+andyd71@users.noreply.github.com>
2021-06-01 14:07:33 +0100

    Add files via upload

--- todo_app/js/todo.js
+++ todo_app/js/todo.js
@@ -1,14 +1,11 @@
 /*this function gets the task from input*/
 function get_todos() {
     /*this creates an array of tasks that are inputted*/
-    var todos = new Array;
-    /*this pulls the task that was saved in the web browser memory*/
-    var todos_str = localStorage.getItem('todo');
-    /*if the input is not null then JSON.parse will communicate with the web browser
-    to make the task a JavaScript object*/
+    var todos = new Array[
+    todos_str = localStorage.getItem('todo')];
     if (todos_str !== null) {
         todos = JSON.parse(todos_str);
-    }
+    todos = get_todos();
     return todos;
 }
 /*this function adds the inputted task to the get_todos function array*/
@@ -16,7 +13,7 @@ function add() {
     /*this takes the inputted task and makes a variable of it*/
     var task = document.getElementById('task').value;
 
-    var todos = get_todos();
+    
     /*this adds a task to the end of the array*/
     todos.push(task);
     /*this converts the task input to a JSON string*/
@@ -32,36 +29,23 @@ function remove() {
     todos.splice(id, 1);
     localStorage.setItem('todo', JSON.stringify(todos));
     show();
-
     return false;
-
 }
-
-/*this functions keeps the tasks permanently displayed on the screen*/
 function show() {
     /*this sets the task that was retrieved as a variable*/
     var todos = get_todos();
-
-    /*this sets up the task as an unordered list*/
     var html = '<ul>';
-    /*this displays a task to the list in the order that it is inputted*/
     for (var i = 0; i < todos.length; i++) {
-        /*this also displays the task as a list and creates the button with the "x"*/
         html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
         var buttons = document.getElementsByClassName('remove');
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', remove);
     };   
     };
-    
-    
     html += '</ul>';
-    /*this displays the task as a list*/
     document.getElementById('todos').innerHTML = html;
     
 }
-/*this displays the inputted task when the 'Add Item' button is clicked*/
-document.getElementById('add').addEventListener('click', add);
-/*this will keep the inputs displayed permanently on the screen*/
-
+document.getElementById('add').addEventListener('click', add)
 show();
+}
