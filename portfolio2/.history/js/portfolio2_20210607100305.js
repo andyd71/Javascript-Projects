9af9a7847055f8a6af1b55b677a919d91b904635
andyd71 <anfield250271@hotmail.com>
2021-06-11 21:30:42 +0100

    updates

---
+++ portfolio2/.history/js/portfolio2_20210607100305.js
@@ -0,0 +1,25 @@
+//These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    this.close(); FIX
+}
+
+//This function covers the slideshow.
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    var dots =
+}
